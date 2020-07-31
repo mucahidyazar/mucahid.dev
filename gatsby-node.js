11ed8ce261faab493d82f05884e2380319f7827b
mucahidyazar <mucahidyazar@gmail.com>
2020-07-31 13:34:29 +0300

    Fixed a problem

--- gatsby-node.js
+++ gatsby-node.js
@@ -44,3 +44,13 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const config = getConfig()
+  if (stage.startsWith("develop") && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "react-dom": "@hot-loader/react-dom",
+    }
+  }
+}
