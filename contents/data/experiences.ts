3e5f4696ed7811f3da34842657166166cfe855a2
Mucahid <mucahidyazar@gmail.com>
2021-02-12 00:02:35 +0300

    Added new experiences and edited experience page

--- contents/data/experiences.ts
+++ contents/data/experiences.ts
@@ -1,6 +1,8 @@
 //SVG FILES
 import GranobraSvg from "../../contents/svg/experiences/granobra.svg"
 import BiletsepetiSvg from "../../contents/svg/experiences/biletspeti.svg"
+import MaxithingsSvg from "../../contents/svg/experiences/maxithings.svg"
+import GetirSvg from "../../contents/svg/experiences/getir.svg"
 
 interface experienceInterface {
   company: string
@@ -17,23 +19,44 @@ export const experiences: experienceInterface[] = [
   {
     company: "GRANOBRA",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi corporis dicta ipsum magni maxime, mollitia quae quaerat veritatis voluptate voluptatem! Accusantium dicta illum impedit quam.",
+      "GRANOBRA şirketinde fullstack developer olarak çalıştım. 1 Full Time proje, 2 side uygulama üzerinde çalıştım.",
     position: "Full Stack Developer",
-    situtation: "Working",
+    situtation: "Done",
     from: "01/03/2020",
-    to: "",
+    to: "01/09/2020",
     website: "https://granobra.com",
     image: GranobraSvg,
   },
   {
     company: "BiletSepeti.com",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi corporis dicta ipsum magni maxime, mollitia quae quaerat veritatis voluptate voluptatem! Accusantium dicta illum impedit quam.",
+      "Granobra'da çalışırken full time olarak çalıştığım turizm e-ticaret sitesidir. React, Next.js, Redux, Sass gibi teklonojiler kullanarak geliştirdim. Figma, XD ve Zeplin üzerinden gelen tasarımları pixelperfect olarak hayata geçiriyordum. Fakat pandemiden dolayı projeyi tamamlayamadan ayrılmak zorunda kaldım.",
     position: "Frontend Developer",
-    situtation: "Working",
+    situtation: "Done",
     from: "01/03/2020",
-    to: "",
+    to: "01/09/2020",
     website: "https://www.biletsepeti.com",
     image: BiletsepetiSvg,
+  },
+  {
+    company: "Maxithings",
+    description:
+      "Maxithings firmasında 3 aylık bir çalışma sürecim oldu. Bu süreçte 1 proje üzerinde tam zamanlı olarak çalıştım. Frontend tarafı tamamen benim tarafımdan yapılmış olan iyifiyat.com e-ticaret sitesini geliştirdim.",
+    position: "Frontend Developer",
+    situtation: "Done",
+    from: "15/10/2020",
+    to: "05/02/2021",
+    website: "https://maxithings.com/",
+    image: MaxithingsSvg,
+  },
+  {
+    company: "Maxithings",
+    description: "Getir firmasında web developer olarak çalışmaya başladım.",
+    position: "Frontend Developer",
+    situtation: "Working",
+    from: "11/02/2021",
+    to: "",
+    website: "https://getir.com/",
+    image: GetirSvg,
   },
 ]
