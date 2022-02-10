0413d33c9aaa218fba1cd648b119a1b0ad5cb4f6
mucahidyazar <mucahidyazar@gmail.com>
2022-02-10 09:20:21 +0300

    fix:

--- pages/api/send/email/index.ts
+++ pages/api/send/email/index.ts
@@ -9,51 +9,56 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const {title, content, type, to = 'mucahidyazar@gmail.com'} = req.body
+  try {
+    const {title, content, type, to = 'mucahidyazar@gmail.com'} = req.body
 
-  const session: any = await getSession({req})
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  } as any)
-
-  if (session?.user?.email) {
-    transporter
-      .sendMail({
-        from: 'admin@mucahid.dev',
-        to,
-        subject: title,
-        text: content,
-        html: `<h1>${title}</h1><p>${content}</p>`,
-      })
-      .then(info => {
-        res.json({info})
-      })
-      .catch(error => {
-        res.json({error})
-      })
-
-    const result = await prisma.message.create({
-      data: {
-        title: title,
-        type: type,
-        content: content,
-        author: {connect: {email: session?.user?.email}},
+    const session: any = await getSession({req})
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
       },
-    })
-    res.json(result)
-  } else {
-    const result = await prisma.message.create({
-      data: {
-        title: title,
-        type: type,
-        content: content,
-      },
-    })
-    res.json(result)
+    } as any)
+
+    if (session?.user?.email) {
+      transporter
+        .sendMail({
+          from: 'admin@mucahid.dev',
+          to,
+          subject: title,
+          text: content,
+          html: `<h1>${title}</h1><p>${content}</p>`,
+        })
+        .then(info => {
+          res.json({info})
+        })
+        .catch(error => {
+          res.json({error})
+        })
+
+      const result = await prisma.message.create({
+        data: {
+          title: title,
+          type: type,
+          content: content,
+          author: {connect: {email: session?.user?.email}},
+        },
+      })
+      res.json(result)
+    } else {
+      const result = await prisma.message.create({
+        data: {
+          title: title,
+          type: type,
+          content: content,
+        },
+      })
+      res.json(result)
+    }
+  } catch (error) {
+    console.log(error)
+    res.json({error})
   }
 }
