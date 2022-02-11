e08cd0ddfc928de2749858d100db7de184bf84d3
mucahidyazar <mucahidyazar@gmail.com>
2022-02-11 09:41:30 +0300

    fix: create a clean yarn.lock, delete a comment line, edit deploy.yml

--- pages/api/send/email/index.ts
+++ pages/api/send/email/index.ts
@@ -4,7 +4,6 @@ import nodemailer from 'nodemailer'
 
 import prisma from '../../../../lib/prisma'
 
-// PUT /api/publish/:id
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse,
