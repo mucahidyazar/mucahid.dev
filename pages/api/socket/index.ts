9abaa229e22bf087feb64fd2f1e9d7dd549f75af
mucahidyazar <mucahidyazar@gmail.com>
2022-02-13 15:10:02 +0300

    fix: for socket connection

--- pages/api/socket/index.ts
+++ pages/api/socket/index.ts
@@ -5,32 +5,32 @@ import {getSession} from 'next-auth/react'
 import {Server as IO} from 'socket.io'
 
 export default function handler(req: NextApiRequest, res: any) {
-  if (!res.socket.server.io) {
-    console.log('Starting Socket.IO server')
+  // if (!res.socket.server.io) {
+  console.log('Starting Socket.IO server')
 
-    const httpServer = res.socket.server as unknown as Server
-    const io = new IO(httpServer, {
-      path: '/api/socket',
-    })
+  const httpServer = res.socket.server as unknown as Server
+  const io = new IO(httpServer, {
+    path: '/api/socket',
+  })
 
-    // Run when client connnects
-    io.on('connection', async socket => {
-      const session = await getSession({req: socket.request})
+  // Run when client connnects
+  io.on('connection', async socket => {
+    const session = await getSession({req: socket.request})
+
+    if (session?.user) {
+      io.emit('USER_CONNECTED', {user: session.user})
+    }
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected')
 
       if (session?.user) {
-        io.emit('USER_CONNECTED', {user: session.user})
+        io.emit('USER_DISCONNECTED', {user: session.user})
       }
-
-      socket.on('disconnect', () => {
-        console.log('user disconnected')
-
-        if (session?.user) {
-          io.emit('USER_DISCONNECTED', {user: session.user})
-        }
-      })
     })
+  })
 
-    res.socket.server.io = io
-  }
-  res.end()
+  //   res.socket.server.io = io
+  // }
+  // res.end()
 }
