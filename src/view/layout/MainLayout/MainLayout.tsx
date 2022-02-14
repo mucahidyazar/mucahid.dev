d32defcbb2089b97abfbd1b3d332e10bf90d35d6
mucahidyazar <mucahidyazar@gmail.com>
2022-02-14 15:37:59 +0300

    refactor: remove socket from next.js app

--- src/view/layout/MainLayout/MainLayout.tsx
+++ src/view/layout/MainLayout/MainLayout.tsx
@@ -1,12 +1,12 @@
-import React, {useCallback, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
 import Head from 'next/head'
-import io, {Socket} from 'socket.io-client'
+// import io, {Socket} from 'socket.io-client'
 import {useDispatch} from 'react-redux'
 
 import {ActiveUsers, SubscribeBanner, Welcome} from '@/components'
 import {Navbar, Footer} from '@/layout'
-import {addActiveUser, removeActiveUser} from '@/store/general'
+// import {addActiveUser, removeActiveUser} from '@/store/general'
 import {setTheme} from '@/store/settings'
 
 import * as S from './style'
@@ -25,33 +25,32 @@ const MainLayout: React.FC<IMainLayout> = ({
   hasWelcome,
 }) => {
   const dispatch = useDispatch()
-  const [socket, setSocket] = React.useState<Socket>()
 
-  const initSocket = useCallback(() => {
-    let socket = io({
-      path: '/api/socket',
-    })
+  // const initSocket = useCallback(() => {
+  //   let socket = io({
+  //     path: '/api/socket',
+  //   })
 
-    // const {'CHAT_MESSAGE', 'ROOM_USERS'} = SOCKET_EVENTS
+  //   // const {'CHAT_MESSAGE', 'ROOM_USERS'} = SOCKET_EVENTS
 
-    // listens for incoming message from server
-    // 1. chat messages
-    socket.on('USER_CONNECTED', ({user}) => {
-      dispatch(addActiveUser(user))
-    })
-    // 2. set room users
-    socket.on('USER_DISCONNECTED', ({user}) => {
-      dispatch(removeActiveUser(user))
-    })
+  //   // listens for incoming message from server
+  //   // 1. chat messages
+  //   socket.on('USER_CONNECTED', ({user}) => {
+  //     dispatch(addActiveUser(user))
+  //   })
+  //   // 2. set room users
+  //   socket.on('USER_DISCONNECTED', ({user}) => {
+  //     dispatch(removeActiveUser(user))
+  //   })
 
-    setSocket(socket)
-  }, [dispatch])
+  //   setSocket(socket)
+  // }, [dispatch])
 
-  useEffect(() => {
-    if (!socket) {
-      initSocket()
-    }
-  }, [socket, initSocket])
+  // useEffect(() => {
+  //   if (!socket) {
+  //     initSocket()
+  //   }
+  // }, [socket, initSocket])
 
   useEffect(() => {
     dispatch(setTheme())
