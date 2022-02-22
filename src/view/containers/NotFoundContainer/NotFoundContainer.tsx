2e44e8fb9c56aa19805ea4229dd81b40821ece2a
mucahidyazar <mucahidyazar@gmail.com>
2022-02-22 20:14:08 +0300

    perf: deleted unnecesary files and use dynamic structure for loties

--- src/view/containers/NotFoundContainer/NotFoundContainer.tsx
+++ src/view/containers/NotFoundContainer/NotFoundContainer.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import type {NextComponentType} from 'next'
-import Lottie from 'react-lottie'
 import Link from 'next/link'
+import dynamic from 'next/dynamic'
 
 import * as S from './style'
-import animationData from './not-found.json'
+
+const DynamicLottie = dynamic(() => import('react-lottie'))
 
 const NotFoundContainer: NextComponentType = () => {
+  const [animationData, setAnimationData] = useState()
+
+  const dynamicAnimation = useCallback(async () => {
+    const animation = await import(`../../../../public/lotties/not-found.json`)
+    return animation
+  }, [])
+
+  useEffect(() => {
+    dynamicAnimation().then(animation => setAnimationData(animation as any))
+  }, [dynamicAnimation])
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -19,7 +31,7 @@ const NotFoundContainer: NextComponentType = () => {
   return (
     <S.NotFoundContainer>
       <S.LottieWrapper>
-        <Lottie
+        <DynamicLottie
           options={defaultOptions}
           width="100%"
           isStopped={false}
