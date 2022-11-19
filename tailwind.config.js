352e786ca41f5758d38ed5d5a9e053d6a81cd27e
Mucahid Yazar <mucahidyazar@gmail.com>
2022-11-19 18:53:38 +0300

    feat: [v3.0.0] landed v3.0.0 with new code base and next.js version 13

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: '#facc15',
+      },
+      screens: {
+        widescreen: {
+          raw: '(min-aspect-ratio: 3/2)',
+        },
+        tallscreen: {
+          raw: '(max-aspect-ratio: 13/20)',
+        },
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': {transform: 'scaleY(0)'},
+          '80%': {transform: 'scaleY(1.2)'},
+          '100%': {transform: 'scaleY(1)'},
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.3s ease-in-out forwards',
+      },
+    },
+  },
+  plugins: [],
+}
