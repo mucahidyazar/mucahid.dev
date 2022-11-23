372e1a587b163b846c9d38a5d3498d92ce87c56e
Mucahid Yazar <mucahidyazar@gmail.com>
2022-11-23 08:17:56 +0300

    fix: build errors

--- app/feeds/page.tsx
+++ app/feeds/page.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
-import Image from 'next/image'
+/* eslint-disable @next/next/no-sync-scripts */
 import Script from 'next/script'
 
 const comments = [
