7cc25b1e59aed7e3d3dd827ceff85e12b837a421
Mucahid Yazar <mucahidyazar@gmail.com>
2022-11-23 08:10:17 +0300

    feat: add feed and room pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,12 +15,13 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className={`${inter.className} text-gray-300`}>
-        <header className="mx-auto w-fit my-20 p-4 sm:p-0">
+      <body className={`${inter.className} text-gray-300 overflow-x-hidden`}>
+        <header className="mx-auto w-fit mt-20 mb-8 p-4 sm:p-0">
           <Image src="/svg/full-logo.svg" alt="logo" width={400} height={80} />
+          <Navbar />
         </header>
         <main className="max-w-4xl mx-auto lg:p-0 px-4">
-          <div className="sm:w-2/3 flex flex-col gap-4 mb-8">
+          <div className="sm:w-2/3 flex flex-col gap-8 mb-8">
             <p>
               Hey, I am Mucahid Yazar, a fanatical middle earth lover, a
               passionate software developer, a curious learner and a human
@@ -29,10 +30,13 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
                 @smartgift
               </a>
             </p>
+            <div className="w-full h-[1px] bg-white opacity-10"></div>
           </div>
-          <Navbar />
           {children}
         </main>
+        <footer className="max-w-4xl mx-auto py-8">
+          <div className="w-full h-[1px] bg-white opacity-10"></div>
+        </footer>
       </body>
     </html>
   )
