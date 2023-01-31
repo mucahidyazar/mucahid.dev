010b5ad56b0101af2fce1a455947b4c58debf354
Mucahid Yazar <mucahidyazar@gmail.com>
2023-01-31 22:32:14 +0300

    fix: post patch and put

--- src/request/utils.ts
+++ src/request/utils.ts
@@ -6,12 +6,12 @@ export const get = <T>(url: string): Promise<T> =>
   api.get(url).then(res => res.data)
 
 export const post = (url: string, {arg}: any) =>
-  api.post(url, {body: arg}).then(res => res.data)
+  api.post(url, arg).then(res => res.data)
 
 export const patch = (url: string, {arg}: any) =>
-  api.patch(url, {body: arg}).then(res => res.data)
+  api.patch(url, arg).then(res => res.data)
 
 export const put = (url: string, {arg}: any) =>
-  api.put(url, {body: arg}).then(res => res.data)
+  api.put(url, arg).then(res => res.data)
 
 export const del = (url: string) => api.delete(url).then(res => res.data)
