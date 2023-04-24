606a4db12a095aba16018caf07408259fcd7127c
Mucahid Yazar <mucahidyazar@gmail.com>
2023-04-24 07:57:47 +0300

    fix: api url

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  API_URL: z.string().default('http://localhost:8001'),
+  API_URL: z.string(),
   NEXT_PUBLIC_API_URL: z.string().default('http://localhost:8001'),
   VERSION: z.string().default('v1'),
   DASHBOARD: z.string().default('admin'),
