dea4fab4327fdf32e6ec940b5ffb991645acd8e4
Mucahid Yazar <mucahidyazar@gmail.com>
2023-11-27 08:33:46 +0300

    fix: update change frequency in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
             {
                   url: 'https://mucahid.dev',
                   lastModified: new Date(),
-                  changeFrequency: 'monthly',
+                  changeFrequency: 'weekly',
                   priority: 1,
             },
             {
